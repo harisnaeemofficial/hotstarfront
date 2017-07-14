457ef98bea86c2c120121b2ccecde0acceab958e
Anil <anil@3dvirtuallabs.com>
2017-07-14 13:44:40 +0530

    frontend first app

--- src/app/containers/app-navbar/app-navbar-menu/app-navbar-menu.component.spec.ts
+++ src/app/containers/app-navbar/app-navbar-menu/app-navbar-menu.component.spec.ts
@@ -14,6 +14,7 @@ describe('AppNavbarMenuComponent', () => {
   }));
 
   beforeEach(() => {
+  
     fixture = TestBed.createComponent(AppNavbarMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
